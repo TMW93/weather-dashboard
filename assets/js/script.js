f6e7a75a629c8c6aac9a468449f1abff447aed31
TimWong <timwong.tw93@gmail.com>
2024-04-09 23:10:32 +1000

    now loops to create 5 days of data

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,38 +13,35 @@ function saveCity(city) {
 }
 
 function createCards(city) {
-  let name = city.city.name;
-  let dateUnix = dayjs.unix(city.list[0].dt);
-  let date = dayjs(dateUnix).format(`DD/MM/YYYY`);
-  let temp = city.list[0].main.temp;
-  let humidity = city.list[0].main.humidity;
-  let wind = city.list[0].wind.speed;
-  // console.log(name);
-  // console.log(date);
-  // console.log(temp);
-  // console.log(humidity);
-  // console.log(wind);
 
-  let cardBodyEl = document.createElement(`div`);
+  for(let i = 0; i < maxDays; i++) {
+    let name = city.city.name;
+    let dateUnix = dayjs.unix(city.list[i].dt);
+    let date = dayjs(dateUnix).format(`DD/MM/YYYY`);
+    let temp = city.list[i].main.temp;
+    let humidity = city.list[i].main.humidity;
+    let wind = city.list[i].wind.speed;
 
-  let cityNameEl = document.createElement(`span`);
-  cityNameEl.textContent = name + date;
-  cardBodyEl.appendChild(cityNameEl);
+    let cardBodyEl = document.createElement(`div`);
 
-  let cityTempEl = document.createElement(`span`);
-  cityTempEl.textContent = `Temperature: ` + temp;
-  cardBodyEl.appendChild(cityTempEl);
-
-  let cityWindEl = document.createElement(`span`);
-  cityWindEl.textContent = `Wind: ` + wind;
-  cardBodyEl.appendChild(cityWindEl);
-
-  let cityHumidityEl = document.createElement(`span`); 
-  cityHumidityEl.textContent = `Humidity: ` + humidity;
-  cardBodyEl.appendChild(cityHumidityEl);
-
-  cardContainerEl.appendChild(cardBodyEl);
-
+    let cityNameEl = document.createElement(`span`);
+    cityNameEl.textContent = name + date;
+    cardBodyEl.appendChild(cityNameEl);
+  
+    let cityTempEl = document.createElement(`span`);
+    cityTempEl.textContent = `Temperature: ` + temp;
+    cardBodyEl.appendChild(cityTempEl);
+  
+    let cityWindEl = document.createElement(`span`);
+    cityWindEl.textContent = `Wind: ` + wind;
+    cardBodyEl.appendChild(cityWindEl);
+  
+    let cityHumidityEl = document.createElement(`span`); 
+    cityHumidityEl.textContent = `Humidity: ` + humidity;
+    cardBodyEl.appendChild(cityHumidityEl);
+  
+    cardContainerEl.appendChild(cardBodyEl);
+  }
 }
 
 function getInfo(city) {
