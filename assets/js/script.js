e59b4c23657d8dd35a8f1681dbc23a66187e90f6
TimWong <timwong.tw93@gmail.com>
2024-04-13 17:50:10 +1000

    previous cards deleted properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ const cardContainer = document.getElementById(`card-container`);
 const cardContainerMainEl = document.getElementById(`card-container-today`);
 const cardContainerFiveDayEl = document.getElementById(`card-container-five-day`);
 const savedCities = document.getElementById(`saved-cities`);
+const cityButtons = document.getElementsByClassName(`city-button`);
+
 const cardLimit = 6;
 
 let cities = JSON.parse(localStorage.getItem(`cities`));
@@ -22,9 +24,13 @@ function saveCity(city) {
 }
 
 function createButtons(cities) {
+  // let buttonId = generateButtonId();
+  // console.log(buttonId);
   let cityButton = document.createElement(`button`);
+  // cityButton.setAttribute(`id`, buttonId);
   cityButton.textContent = cities;
-  cityButton.classList = `d-block p-2`;
+  cityButton.classList = `city-button d-block p-2`;
+  cityButton.setAttribute(`type`, `button`);
   savedCities.appendChild(cityButton);
   // console.log(cityButton.textContent);
 }
@@ -86,25 +92,35 @@ function createCards(city) {
 
       //set the current date to the previous date variable
       previousDate = currentDate;
-
-      // console.log(`This is previous date: ${previousDate}`);
-      // console.log(`This is current date: ${currentDate}`);
-      // if(previousDate === currentDate) {
-      //   console.log(`it matches`);
     }
   }
 }
 
 function getInfo(city) {
+  let found = 0;
+
   const apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=metric`;
+  
   fetch(apiUrl)
     .then(function(response) {
       if(response.ok) {
         response.json().then(function(data) {
+          // console.log(data);
           //save city name
-          cities.push(data.city.name);
-          saveCity(cities);
+          for(let i = 0; i < cities.length; i++) {
+            if(city === cities[i]) {
+              found = 1;
+            } 
+          }
+
+          if(found === 0) {
+            cities.push(data.city.name);
+            saveCity(cities);
+            createButtons(city);
+          }
+
           createCards(data);
+
         });
       } else {
         alert(`Error: ${response.statusText}`);
@@ -115,32 +131,60 @@ function getInfo(city) {
     });
 }
 
+function clearCards() {
 
-
-formEl.addEventListener(`submit`, function(event) {
-  event.preventDefault();
-
-  if(inputEl.value) {
-    let city = inputEl.value;
-    //API is case sensitive, so checking if input starts with an uppercase
-    if(city[0] === city[0].toLowerCase()) {
-      city = capitaliseLetter(city);
+  if(cardContainerMainEl.hasChildNodes) {
+    if(cardContainerMainEl.childNodes.length > 0) {
+      for(let i = 0; i < cardContainerMainEl.childNodes.length; i++) {
+        cardContainerMainEl.children[i].remove();
+      }
     }
-    inputEl.value = ``;
-    getInfo(city);
-    createButtons(city);
-  } else {
-    alert(`enter something`);
   }
 
-});
+
+  if(cardContainerFiveDayEl.hasChildNodes) {
+    if(cardContainerFiveDayEl.childNodes.length > 0) {
+      let length = cardContainerFiveDayEl.childNodes.length;
+      for(let i = 0; i < length; i++) {
+        cardContainerFiveDayEl.firstElementChild.remove();
+        // console.log(cardContainerFiveDayEl.children.length);
+        // console.log(cardContainerFiveDayEl.children);
+      }
+    }
+  }
+
+}
+
 
 $(document).ready(function() {
-  console.log(`document ready`);
+  // console.log(`document ready`);
 
   if(cities != null) {
     for(let i = 0; i < cities.length; i++) {
       createButtons(cities[i]);
+      cityButtons[i].addEventListener(`click`, function(event) {
+        // console.log(event.target.textContent);
+        clearCards();
+        getInfo(event.target.textContent);
+      });
     }
   }
+
+
+
+  formEl.addEventListener(`submit`, function(event) {
+    event.preventDefault();
+  
+    if(inputEl.value) {
+      let city = inputEl.value;
+      //API is case sensitive, so checking if input starts with an uppercase
+      if(city[0] === city[0].toLowerCase()) {
+        city = capitaliseLetter(city);
+      }
+      inputEl.value = ``;
+      getInfo(city);
+    } else {
+      alert(`enter something`);
+    }
+  });
 });
